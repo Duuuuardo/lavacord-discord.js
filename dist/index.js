a59c747f631780057674027278ba57f7438c899b
Duuuuardo <79720101+Duuuuardo@users.noreply.github.com>
2022-02-20 11:48:45 -0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -31,7 +31,7 @@ class Manager extends lavacord_1.Manager {
         });
         client.ws
             .on("VOICE_SERVER_UPDATE", (packet) => this.voiceServerUpdate({ guild_id: packet.guild_id }))
-            .on("VOICE_STATE_UPDATE", (packet) => => this.voiceStateUpdate({ user_id: packet.user_id, channel_id: packet.channel_id, guild_id: packet.guild_id }))
+            .on("VOICE_STATE_UPDATE", (packet) => this.voiceStateUpdate({ user_id: packet.user_id, channel_id: packet.channel_id, guild_id: packet.guild_id }))
             .on("GUILD_CREATE", async (data) => {
             for (const state of data.voice_states)
                 await this.voiceStateUpdate({ ...state, guild_id: data.id });
